d18c367d6b3fc3cb24bddda33f0d1c6203b7e634
Blairi <axel.avi.fer@gmail.com>
2026-03-10 15:28:38 -0600

    cambiar traje aleatoriamente

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -1,25 +1,21 @@
-using System.Collections;
+using System;
 using System.Diagnostics;
 using UnityEngine;
 
 public class ChangeMaterial : MonoBehaviour
 {
     public GameObject model;
-    public Material trajeMaterial;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
+    public Material[] materialesTraje;
 
     public void ChangeColor_BTN()
     {
+        // Validamos que haya materiales en la lista y que el modelo exista
+        if (materialesTraje.Length == 0 || model == null) return;
+
+        // Obtenemos el Renderer del modelo
         Renderer rend = model.GetComponentInChildren<Renderer>();
-        rend.material = trajeMaterial;
+        int indiceAleatorio = UnityEngine.Random.Range(0, materialesTraje.Length);
+        rend.material = materialesTraje[indiceAleatorio];
     }
 }
